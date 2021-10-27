ec79ba0951391deba74d153f480e0aa3fc09c637
Svetlana-Pletneva <bg026@yandex.ru>
2021-10-28 00:18:12 +0500

    Задание 3

--- Home_work/OOP/OOP.py
+++ Home_work/OOP/OOP.py
@@ -20,6 +20,26 @@ class Student:
         else:
             'Ошибка'
 
+        # подсчет средней оценки студента
+    def rating(self):
+            sum = 0
+            quantity = 0
+            if len(self.grades) != 0:
+                for val in self.grades.values():
+                    for grade in val:
+                        sum += grade
+                        quantity += 1
+                        average = sum / quantity
+                        return average
+            else:
+                return 0
+
+    def __str__(self):
+        return f'Имя: {self.name} \nФамилия: {self.surname} \n' \
+               f'Средняя оценка за домашнии задания: {self.rating()} \n' \
+               f'Курсы в процессе обучения: {self.courses_in_progress} \n' \
+               f'Завершённые курсы: {self.finished_courses}'
+
 
 # Класс преподаватели
 class Mentor:
@@ -34,6 +54,22 @@ class Lecturer(Mentor):
         self.courses_attached = []  # Курсы преподавателя
         self.grades = {}
 
+    def __str__(self):
+        return f'Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за лекции: {self.medium_grades()}'
+
+    def medium_grades(self):
+        sum = 0
+        quantity = 0
+        if len(self.grades) != 0:
+            for val in self.grades.values():
+                for grade in val:
+                    sum += grade
+                    quantity += 1
+                    average = sum / quantity
+                return average
+        else:
+            return 0
+
 class Reviewer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
@@ -47,3 +83,51 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+    def __str__(self):
+        return f'Имя: {self.name} \n Фамилия: {self.surname} \n'
+# студент_1
+first_student = Student('Ruoy', 'Eman', 'boy')
+first_student.courses_in_progress += ['Python']
+first_student.finished_courses += ['Введение в программирование']
+
+# студент_2
+second_student = Student('Bob', 'Grenn', 'girl')
+second_student.courses_in_progress += ['Geology']
+
+# проверяющий_1
+first_reviewer = Reviewer('Svetlana', 'Pletneva')
+first_reviewer.courses_attached += ['Python']
+
+# проверяющий_2
+second_reviewer = Reviewer('Some', 'Buddy')
+second_reviewer.courses_attached += ['Python']
+second_reviewer.rate_hw(first_student, 'Python', 10)
+second_reviewer.rate_hw(first_student, 'Python', 10)
+second_reviewer.rate_hw(second_student, 'Geology', 8)
+
+# лектор_1
+first_lecturer = Lecturer('Artur', 'Red')
+
+# лектор_2
+second_lecturer = Lecturer('Sam', 'Smith')
+second_lecturer.courses_attached += ['Geology']
+
+# оцениваем лектора_2
+second_student.grade_for_lectur(second_lecturer, 'Geology', 9)
+second_student.grade_for_lectur(second_lecturer, 'Geology', 8)
+
+
+print(second_reviewer, '\n')
+print(second_lecturer, '\n')
+print(first_student, '\n')
+
+print(first_student.grades)
+print(second_lecturer.grades)
+
+
+
+
+
+
+
+
